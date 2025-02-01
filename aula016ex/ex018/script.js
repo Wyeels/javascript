8a6642c6346264856e2bc06c7d8213bff61a4b12
Wesley Oliveira Xavier <143528390+Wyeels@users.noreply.github.com>
2025-02-01 20:54:27 -0300

    Encerrando o proximo passo

--- aula016ex/ex018/script.js
+++ aula016ex/ex018/script.js
@@ -1,5 +1,6 @@
 let botaoa = window.document.getElementById('iadd')
 let botaof = window.document.getElementById('iend')
+let salvos = []
 
 botaoa.addEventListener('click', start)
 
@@ -8,14 +9,12 @@ botaof.addEventListener('click', end)
 function start() {
 let numero = document.getElementById('inumero').value 
 
-let salvos = []
-
 let valores = document.getElementById('ivalores')
 
 if (numero < 1 || numero > 100 || numero.length < 1) {
     alert('[ERRO] Por favor, verifique se o valor que você quer adicionar é valido!')
 } else if (salvos.indexOf(numero) != -1) {
-    alert('Erro')
+    alert('[ERRO] O valor escolhido já foi selecionado!')
 } else {
     let opcao = document.createElement('option')
     valores.appendChild(opcao)
@@ -24,11 +23,33 @@ if (numero < 1 || numero > 100 || numero.length < 1) {
     salvos.sort((a, b) => a - b)
     return salvos
     }
-
-
 }
 
 function end() {
-    document.writeln(salvo)
+    let textos = window.document.createElement('div')
+
+    document.body.appendChild(textos)
+
+    textos.innerText = ''
+    if (salvos.length < 1) {
+        alert('[ERRO] Por favor, garanta que você tenha adicionado pelo menos um número!')
+    } else {
+        for (let soma = 0; soma < salvos.length; soma++) {
+            var fim = 0 
+            fim = fim += salvos[soma]
+            
+            
+            textos.innerText = fim
+        }
+
+        textos.innerText = `
+        No total, foram cadastrados ${salvos.length} números
+        O maior número escolhido foi ${salvos[salvos.length - 1]}
+        O menor número da lista é o ${salvos[0]}
+        A soma de todos esses valores é de ${fim}
+        A média de todos os valores juntos é de ${salvos}`
+    } 
+    
+    
 }
 
