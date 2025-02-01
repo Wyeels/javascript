05c1745bf30a9d0c8d72ed4b5d8cdd9004b13dee
Wesley Oliveira Xavier <143528390+Wyeels@users.noreply.github.com>
2025-02-01 12:55:35 -0300

    fazendo melhorias

--- aula016ex/ex018/script.js
+++ aula016ex/ex018/script.js
@@ -8,18 +8,27 @@ botaof.addEventListener('click', end)
 function start() {
 let numero = document.getElementById('inumero').value 
 
+let salvos = []
+
 let valores = document.getElementById('ivalores')
 
 if (numero < 1 || numero > 100 || numero.length < 1) {
     alert('[ERRO] Por favor, verifique se o valor que você quer adicionar é valido!')
+} else if (salvos.indexOf(numero) != -1) {
+    alert('Erro')
 } else {
     let opcao = document.createElement('option')
     valores.appendChild(opcao)
-    opcao.innerText = `O valor ${numero} foi adicionado!` 
-    if (opcao.value == numero) {
-        alert('erro')
+    opcao.innerText = `O valor ${numero} foi adicionado!`
+    salvos.push(numero)
+    salvos.sort((a, b) => a - b)
+    return salvos
     }
+
+
 }
 
+function end() {
+    document.writeln(salvo)
 }
 
