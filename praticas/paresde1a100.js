6b53178eba366188fc81decfdc3fb3ebfe17d48b
Wesley Oliveira Xavier <143528390+Wyeels@users.noreply.github.com>
2025-02-10 22:40:13 -0300

    Update paresde1a100.js

--- praticas/paresde1a100.js
+++ praticas/paresde1a100.js
@@ -1,19 +1,10 @@
-// function paresDe1a100() {
-//     for (let contador = 1; contador <= 100; contador++) {
-//     if (contador % 2 == 0) {
-//         console.log(contador)
-//     }
-//     }
-// }
-
-// paresDe1a100()
-
-function somaArray(valores) {
-    let total = 0
-    for (let numeros of valores) {
-        total += numeros
+function paresDe1a100() {
+    for (let contador = 1; contador <= 100; contador++) {
+    if (contador % 2 == 0) {
+        console.log(contador)
     }
-    return total
+    }
 }
 
-console.log(somaArray([5, 6, 7, 2, 8, 10]))
+paresDe1a100()
+
