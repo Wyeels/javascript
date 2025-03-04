62eadd4dfd7561b0068078eb73756d53b5a4f89b
Wesley Oliveira Xavier <143528390+Wyeels@users.noreply.github.com>
2025-03-04 16:02:53 -0300

    Update lista.js

--- praticas/desafio-lista/lista.js
+++ praticas/desafio-lista/lista.js
@@ -1,54 +1,48 @@
 const item = document.getElementsByClassName('itens')
-const itens = []
 
 for (let contador in item) {
     const botao = document.createElement('button')
+    botao.setAttribute('class', contador)
     item[contador].after(botao)
     botao.innerText = 'Completo'
     botao.style.display = 'inline'
-    botao.addEventListener('click', clicou)
-    botao.insertAdjacentHTML('afterend', '<br></br>')
+    botao.addEventListener('click', completo)
+    botao.insertAdjacentHTML('afterend', '<br><br>')
     botao.value = contador
 }
 
-function clicou() {
+
+function completo() {
     const escolhido = this.value
     const botesc = this
 
     if (item[escolhido].style.textDecoration == 'line-through') {
         item[escolhido].style.textDecoration = 'none'
         botesc.innerText = 'Completo'
-        itens[botesc].style.background = 'red'
-        
 
-        if (itens.indexOf()) {
-            itens.splice(botesc, 1)
-        }
-
-        if (itens[escolhido].checkVisibility() == true) {
-            
-            itens[escolhido].remove()
-        }
-
-        
+        document.getElementById(escolhido).remove()
 
     } else {
         const eliminar = document.createElement('button')
-        eliminar.setAttribute('class', 'boteli')    
+        eliminar.setAttribute('id', escolhido)  
+        eliminar.value = escolhido 
         eliminar.innerText = 'Apagar'
-        eliminar.value = escolhido
         botesc.after(eliminar)
         item[escolhido].style.textDecoration = 'line-through'
         botesc.innerText = 'Incompleto'
-        itens.push(eliminar.value)
 
-        // itens.push(document.querySelectorAll('.boteli'))
-        
-        // itens.push(document.querySelectorAll('button.boteli').value)
+        eliminar.addEventListener('click', apagar)
     }
+    alert(escolhido)
+}
+
+function apagar() {
+    const escolhido = this.value
+    
+    document.getElementsByClassName(escolhido)[0].remove()
+    document.getElementById(escolhido).remove()
+    item[escolhido].remove()
 
     alert(escolhido)
-    alert((botesc))
-    alert(itens)
-    escolhido.style.background = 'red'
 }
+
