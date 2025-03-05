cea13d39d6c213b2e9493171e180e44fe84baa82
Wesley Oliveira Xavier <143528390+Wyeels@users.noreply.github.com>
2025-03-05 19:04:14 -0300

    resolvendo todos os problemas anteriores e deixando o projeto praticamente funcional

--- praticas/desafio-lista/lista.js
+++ praticas/desafio-lista/lista.js
@@ -1,44 +1,70 @@
-const item = document.getElementsByClassName('itens')
 const section = document.querySelector('section#guardaritem')
 section.style.height = 'min-content'
+
 const novoitem = document.createElement('button')
 novoitem.innerText = 'Criar'
 section.appendChild(novoitem)
 novoitem.addEventListener('click', criar)
-carregar()
 
-function carregar() {
+function criar() {
+    const paragrafo = document.createElement('p')
+    const caixa = document.createElement('input')
+    caixa.setAttribute('class', 'itens')
+    paragrafo.appendChild(caixa)
+    novoitem.before(paragrafo)
+    atualizar()
+}
+
+const item = document.getElementsByClassName('itens')
+
+atualizar()
+
+function atualizar() {
+    const paragrafo = document.querySelectorAll('p')
+
     for (let contador in item) {
-    const botao = document.createElement('button')
-    botao.setAttribute('class', contador)
-    item[contador].after(botao)
-    botao.innerText = 'Completo'
-    botao.style.display = 'inline'
-    botao.addEventListener('click', completo)
-    botao.insertAdjacentHTML('afterend', '<br><br>')
-    botao.value = contador
+        paragrafo[contador].setAttribute('class', `a${contador}`)
+        const label = document.createElement('label')
+        label.setAttribute('class', contador)
+        const botao = document.createElement('button')
+        item[contador].setAttribute('id', contador)
+        item[contador].after(label)
+        label.appendChild(botao)
+        botao.innerText = 'Completo'
+        botao.style.display = 'inline'
+        botao.addEventListener('click', completo)
+        botao.value = contador
+        if (document.getElementsByClassName(contador)[]) {
+            remove()
+        }
+        
+    }
+    // if (document.getElementsByClassName('1')[1].checkVisibility() == true) {
+    //     // botao[contador].remove()
+    //     alert('a')
+    // }
 }
-}
-
 
 
 function completo() {
     const escolhido = this.value
     const botesc = this
-
-    if (item[escolhido].style.textDecoration == 'line-through') {
-        item[escolhido].style.textDecoration = 'none'
+    const opcoes = document.getElementById(escolhido)
+    // botesc.setAttribute('class', escolhido)
+    const label = document.querySelectorAll('label')
+    if (opcoes.style.textDecoration == 'line-through') {
+        opcoes.style.textDecoration = 'none'
         botesc.innerText = 'Completo'
-
-        document.getElementsByName(escolhido)[0].remove()
+        // document.getElementsByClassName(escolhido)[0].remove()
+        document.getElementsByClassName(escolhido)[0].lastChild.remove()
 
     } else {
         const eliminar = document.createElement('button')
-        eliminar.setAttribute('name', escolhido)  
+        eliminar.style.display = 'inline'  
         eliminar.value = escolhido 
         eliminar.innerText = 'Apagar'
-        botesc.after(eliminar)
-        item[escolhido].style.textDecoration = 'line-through'
+        document.getElementsByClassName(escolhido)[0].appendChild(eliminar)
+        opcoes.style.textDecoration = 'line-through'
         botesc.innerText = 'Incompleto'
 
         eliminar.addEventListener('click', apagar)
@@ -48,17 +74,14 @@ function completo() {
 
 function apagar() {
     const escolhido = this.value
-    
-    document.getElementsByClassName(escolhido)[0].remove()
-    document.getElementsByName(escolhido)[0].remove()
-    document.getElementsByClassName('itens')[0].remove()
+    document.getElementsByClassName(`a${escolhido}`)[0].remove()
 }
 
-function criar() {
-    const caixa = document.createElement('input')
-    caixa.setAttribute('class', 'itens2')
-    caixa.style.display = 'block'
-    novoitem.insertAdjacentElement('beforebegin', caixa)
-}
+// function criar() {
+//     const caixa = document.createElement('p')
+//     caixa.setAttribute('class', 'itens2')
+//     caixa.style.display = 'block'
+//     novoitem.insertAdjacentElement('beforebegin', caixa)
+// }
 
 
